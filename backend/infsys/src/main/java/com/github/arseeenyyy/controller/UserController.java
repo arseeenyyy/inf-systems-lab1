40ec13de7afa516835afefb4db8ef8396ff7e9b7
arseeenyyy <rubtsov113@mail.ru>
2025-11-09 17:33:00 +0300

    remove useless stuff

--- backend/infsys/src/main/java/com/github/arseeenyyy/controller/UserController.java
+++ backend/infsys/src/main/java/com/github/arseeenyyy/controller/UserController.java
@@ -1,5 +1,19 @@
 package com.github.arseeenyyy.controller;
 
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserController {
-    
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
 }
